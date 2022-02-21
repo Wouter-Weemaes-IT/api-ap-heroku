f475d18f46cc11593c669f0e7832f6d7987fac54
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-02-21 21:09:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://xenodochial-gates-cf6795.netlify.app'
+        origin: '*'
     })
 );
 app.options('*', cors());
